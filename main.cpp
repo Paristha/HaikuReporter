d09fc753d9abad25775b07e90efad2a367e1332a
Thana Paris <raphaelparisdi@gmail.com>
2021-01-18 11:13:47 -0500

    Finished refactoring makeRule
    makeRule func complete; getRuleIDs started

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 std::string BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAKNhLAEAAAAAzItOgprPgVhFZIc6JjgD74tIu34%3D4OlxvtQhLZ9p2E5kblp79f6rOOx33L0SZivMJm8prJpTvh5qRC";
-std::vector<int> RULE_IDS;
+std::vector<std::string> RULE_IDS;
 
 
 bool makeRule(std::string value_text, std::string tag_text = "") 
@@ -23,39 +23,77 @@ bool makeRule(std::string value_text, std::string tag_text = "")
 
 	nlohmann::json item = nlohmann::json::parse(r.text);
 
-	if (item.contains("errors")) {
-		std::string error = "Rule(s) not accepted. Errors: " + item["errors"].dump();
+	if (item.contains("errors") || r.status_code != 201) {
+		std::string error = "";
+
+		if (item.contains("errors")) {
+			for (const auto& rule : item["errors"]) {
+				RULE_IDS.push_back(rule["id"].dump());
+			}
+			error += "Rule(s) not accepted. Errors: " + item["errors"].dump() + "\n";
+		}
+
+		if (r.status_code != 201)
+			error += "HTTP Error Code: " + std::to_string(r.status_code);
+
 		throw error;
 		return false;
 	}
+
+	for (const auto& rule : item["data"]) {
+		RULE_IDS.push_back(rule["id"].dump());
+	}
+
+	return true;
+}
+
+bool getRuleIDs() {
+	cpr::Response r = cpr::Get(cpr::Url{ "https://api.twitter.com/2/tweets/search/stream/rules" },
+		cpr::Header{ { "Authorization", "Bearer " + BEARER_TOKEN } });
+
+	nlohmann::json item = nlohmann::json::parse(r.text);
 
 	if (r.status_code != 201) {
-		std::string error = "HTTP Error Code: " + r.status_code;
+		std::string error = "HTTP Error Code: " + std::to_string(r.status_code);
 		throw error;
 		return false;
 	}
 
-	std::vector<int> ids_local;
+	std::vector<std::string> new_rule_ids;
 	for (const auto& rule : item["data"]) {
-		ids_local.push_back(rule["id"]);
+		new_rule_ids.push_back(rule["id"].dump());
 	}
-
-	std::cout << "Rules Added: " << r.text << std::endl;
+	RULE_IDS = new_rule_ids;
 
 	return true;
 }
 
 int main()
 {
+	//try {
+	//	std::string rule = "#breakingnews lang:en -died -death -deaths -kill -killed -killing -killings -shooting -shootings -shot -stabbing -stabbed -fatalities -burned -crash -crashed -rape -raped -sexual -casualties -assault -assaulted -is:retweet";
+	//	std::string tag = "english, non-retweet breaking news without dangerous words";
+	//	makeRule(rule, tag);
+	//	//for (std::vector<std::string>::const_iterator i = RULE_IDS.begin(); i != RULE_IDS.end(); ++i)
+	//	//	std::cout << *i << ' ';
+	//} catch (std::string error) {
+	//	std::cout << error << std::endl;
+	//	std::cout << "\nError ids: ";
+	//	for (std::vector<std::string>::const_iterator i = RULE_IDS.begin(); i != RULE_IDS.end(); ++i)
+	//		std::cout << *i << ", ";
+	//	std::cout << std::endl;
+	//}
+
 	try {
-		std::string rule = "#breakingnews lang:en -died -death -deaths -kill -killed -killing -killings -shooting -shootings -shot -stabbing -stabbed -fatalities -burned -crash -crashed -rape -raped -sexual -casualties -assault -assaulted -is:retweet";
-		std::string tag = "english, non-retweet breaking news without dangerous words";
-		makeRule(rule, tag);
-	} catch (std::string error) {
+		bool func_success = getRuleIDs();
+		if (func_success) {
+			for (std::vector<std::string>::const_iterator i = RULE_IDS.begin(); i != RULE_IDS.end(); ++i)
+				std::cout << *i << ' ';
+		}
+	}
+	catch (std::string error) {
 		std::cout << error << std::endl;
 	}
-
-
 	//std::cout << "Rules Added:" << r1.text << std::endl;
 
 	//cpr::Response r2 = cpr::Get(cpr::Url{ "https://api.twitter.com/2/tweets/search/stream" },
@@ -73,10 +111,7 @@ int main()
 
 	//std::cout << "Bytes:" << r2.downloaded_bytes << std::endl;
 
-	//cpr::Response r3 = cpr::Get(cpr::Url{ "https://api.twitter.com/2/tweets/search/stream/rules" },
-	//	cpr::Header{ { "Authorization", "Bearer " + BEARER_TOKEN } });
 
-	//std::cout << "Rules:" << r3.text << std::endl;
 
 	//auto future_text = cpr::GetCallback([](cpr::Response r) {
 	//	return r.text;
